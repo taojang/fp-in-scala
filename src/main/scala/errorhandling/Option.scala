f6099b5a2d5eca5e04ee4684467685a2d79d72a6
Tao Yang <tao.yang@zalando.de>
2015-02-05 23:05:00 +0100

    chapter 4 option reorganize

--- src/main/scala/errorhandling/Option.scala
+++ src/main/scala/errorhandling/Option.scala
@@ -1,30 +1,32 @@
 package errorhandling
 
-object Chapter4 {
+sealed trait Option[+A] {
 
-  sealed trait Option[+A] {
-
-    def map[B](f: A => B): Option[B] = this match {
-      case None => None
-      case Some(a) => Some(f apply a)
-    }
-
-    def flatMap[B](f: A => Option[B]): Option[B] = this map (f apply _) getOrElse None
-
-    def getOrElse[B >: A](default: => B): B = this match {
-      case Some(a) => a
-      case None => default
-    }
-
-    def orElse[B >: A](ob: => Option[B]): Option[B] = this map { a => Some(a) } getOrElse ob
-
-    def filter(f: A => Boolean): Option[A] = this flatMap { a => if (f apply a) Some(a) else None }
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f apply a)
   }
-  case class Some[+A](get: A) extends Option[A]
-  case object None extends Option[Nothing]
 
-  object Option {
+  def flatMap[B](f: A => Option[B]): Option[B] = this map (f apply _) getOrElse None
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(a) => a
+    case None => default
   }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this map { a => Some(a) } getOrElse ob
+
+  def filter(f: A => Boolean): Option[A] = this flatMap { a => if (f apply a) Some(a) else None }
+}
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+object Option {
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = for {
+    aa <- a
+    bb <- b
+  } yield f(aa, bb)
 
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty)
@@ -35,12 +37,6 @@ object Chapter4 {
     mean(xs).flatMap { m =>
       mean(xs.map(x => math.pow(x - m, 2)))
     }
-
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = for {
-    aa <- a
-    bb <- b
-  } yield f(aa, bb)
-
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] =
     if (a.isEmpty)
