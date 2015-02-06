24459068e0d81688a1c696e77c37985878908624
Tao Yang <tao.yang@zalando.de>
2015-02-06 22:53:12 +0100

    ex. 4.6

--- src/main/scala/errorhandling/Either.scala
+++ src/main/scala/errorhandling/Either.scala
@@ -2,14 +2,29 @@ package errorhandling
 
 trait Either[+E, +A] {
 
-  def map[B](f: A => B): Either[E, B]
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(a) => Right(f apply a)
+    case Left(e)  => Left(e)
+  }
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => f apply a
+    case Left(e) => Left(e)
+  }
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B]
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => Right(a)
+    case Left(e) => b
+  }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C]
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for {
+    aa <- this
+    bb <- b
+  } yield f(aa, bb)
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
+
+object Either {
+}
