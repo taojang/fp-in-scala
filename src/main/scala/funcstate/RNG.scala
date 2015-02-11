2cd902a7f7142b76b2ccf11f8e3fb545907a2d24
Tao Yang <tao.yang@zalando.de>
2015-02-11 23:01:49 +0100

    ex 6.7

--- src/main/scala/funcstate/RNG.scala
+++ src/main/scala/funcstate/RNG.scala
@@ -15,13 +15,47 @@ case class SimpleRNG(seed: Long) extends RNG {
 
 object SimpleRNG {
 
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = { rng =>
+    val (a, r1) = ra(rng)
+    val (b, r2) = rb(r1)
+    (f(a, b), r2)
+  }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra, rb)((_, _))
+
+  val int: Rand[Int] = _.nextInt
+
+  val double: Rand[Double] = map(nonNegativeInt) { i =>
+    if (i == Int.MaxValue) (i - 1) / Int.MaxValue
+    else i / Int.MaxValue
+  }
+
+  /*
+  def double(rng: RNG): (Double, RNG) = nonNegativeInt(rng) match {
+    case (n, r) => if (n == Int.MaxValue) double(r) else (n / Int.MaxValue, r)
+  }
+  */
+
+  val randIntDouble: Rand[(Int, Double)] = both(int, double)
+
+  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+
   def nonNegativeInt(rng: RNG): (Int, RNG) = rng.nextInt match {
     case (n, r) => if (n == Int.MinValue) nonNegativeInt(r) else (Math.abs(n), r)
   }
 
-  def double(rng: RNG): (Double, RNG) = nonNegativeInt(rng) match {
-    case (n, r) => if (n == Int.MaxValue) double(r) else (n / Int.MaxValue, r)
-  }
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
 
   def intDouble(rng: RNG): ((Int, Double), RNG) = {
     val (i, r1) = rng.nextInt
@@ -40,5 +74,28 @@ object SimpleRNG {
     ((d1, d2, d3), r3)
   }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+
+    def intsRecurr(c: Int)(acc: List[Int])(rng: RNG): (List[Int], RNG) = {
+      if (c < 1) (acc, rng)
+      else {
+        val (n, r) = rng.nextInt
+        intsRecurr(c - 1)(n :: acc)(r)
+      }
+    }
+    
+    intsRecurr(count)(Nil)(rng)
+  }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = { rng =>
+    fs match {
+      case Nil => (Nil, rng)
+      case h :: t => {
+        val (a, r) = h(rng)
+        val (a1, r1) = sequence(t)(r)
+        (a :: a1, r1)
+      }
+    }
+  }
+
 }
